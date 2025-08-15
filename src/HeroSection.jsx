efb9a7406d72ada2e71d3afb2cd279f714814daf
HarshiniNatarajan2003 <harshinisubanu@gmail.com>
2025-08-15 17:59:51 +0530

    third

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -7,9 +7,8 @@ export default function HeroSection() {
       <div className="overlay"></div>
 
       <div className="hero-content">
-        <h1>FIND YOUR DREAM<br />PROPERTY TODAY</h1>
-        <p>Find best houses, plots and agriculture lands in and around Tenkasi.</p>
-
+        <h1>FIND YOUR PROPERTY</h1>
+        
         {/* Contact Us button with phone number */}
         <a href="tel:+919999999999" className="contact-btn">
           <FaPhoneAlt /> +91 9999999999
