b53fb4799ff438e2fdb9f2ef8d5d094bb6c390ff
HarshiniNatarajan2003 <harshinisubanu@gmail.com>
2025-08-16 00:32:38 +0530

    seven

--- src/PropertyDetails.jsx
+++ src/PropertyDetails.jsx
@@ -1,4 +1,4 @@
-import { useLocation, useParams, useNavigate } from "react-router-dom";
+import { useLocation,  useNavigate } from "react-router-dom";
 import "./PropertyDetails.css";
 
 export default function PropertyDetails() {
